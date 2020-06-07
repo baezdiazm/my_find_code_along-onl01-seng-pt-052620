41549650ffe4c27057821f8b2ad0830445a6cc90
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-07 20:14:32 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 def my_find(collection)
-counter = 0
-while counter < collection.length
-  counter += 1
-end
+  counter = 0
+  while counter < collection.length
+    counter += 1
+  end
 
 end
